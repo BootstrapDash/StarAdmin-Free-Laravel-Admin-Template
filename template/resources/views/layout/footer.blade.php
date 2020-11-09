4d8a4ef732076f80b085a9ce5c210fce07effb0f
bootstrapdash-team <git@bootstrapdash.com>
2020-11-09 13:17:59 +0530

    footer links

--- template/resources/views/layout/footer.blade.php
+++ template/resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container-fluid clearfix">
-    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2019 <a href="http://www.bootstrapdash.com/" target="_blank">Bootstrapdash</a>. All rights reserved.</span>
-    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i>
+    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©  <a href="http://www.bootstrapdash.com/" target="_blank">bootstrapdash.com</a> 2020</span>
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Starter <a href="https://www.bootstrapdash.com/laravel-templates/" target="_blank">Laravel template</a> by BootstrapDash <i class="mdi mdi-heart text-danger"></i>
     </span>
   </div>
 </footer>
