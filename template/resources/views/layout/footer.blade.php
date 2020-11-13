8cf61461588d15c476eedec3de9a07ce18c564c2
bootstrapdash-team <git@bootstrapdash.com>
2020-11-13 13:49:36 +0530

    copyright text edit

--- template/resources/views/layout/footer.blade.php
+++ template/resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container-fluid clearfix">
     <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©  <a href="http://www.bootstrapdash.com/" target="_blank">bootstrapdash.com</a> 2020</span>
-    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Starter <a href="https://www.bootstrapdash.com/laravel-templates/" target="_blank">Laravel template</a> by BootstrapDash <i class="mdi mdi-heart text-danger"></i>
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Free <a href="https://www.bootstrapdash.com/laravel-templates/" target="_blank">Laravel template</a> by BootstrapDash.com <i class="mdi mdi-heart text-danger"></i>
     </span>
   </div>
 </footer>
